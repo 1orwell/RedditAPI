cdf3655e53f1cbf8c1ba5313bb539a333843c0aa
1orwell <elise@eliseratcliffe.co.uk>
2022-03-26 13:05:24 +0000

    collected title, url, score, and number of comments from top 100 reddit posts and stored them as a .csv file using pandas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import pandas as pd
+from IPython.display import display
 
-subreddit = "python"
+subreddit = "knitting"
 listing = "top" # controversial, best, hot, new, etc.
 limit = 100
 timeframe = "month" #hour, day week, month, day, year, all
@@ -22,6 +24,29 @@ def get_post_titles(r):
         posts.append(x)
     return posts
 
+def get_results(r):
+    #create DataFrame showing title, url, score and number of comments
+
+    my_dict = {}
+    for post in r['data']['children']:
+        x = post['data']['title']
+        my_dict[x] = {'url':post['data']['url'],'score':post['data']['score'],'comments':post['data']['num_comments']}
+    """
+    pd stands for pandas - used for data manipulation and analysis
+    e.g.
+    d = {'col1' : [1, 2], 'col2' : [3, 4]}
+    df = pd.DataFrame(data = d)
+    >> df
+        col1    col2
+    0      1       3
+    1      2       4
+    """
+    df = pd.DataFrame.from_dict(my_dict, orient='index')
+    return(df)
+
 r = get_reddit(subreddit, listing, limit, timeframe)
-posts = get_post_titles(r)
-print(posts)
+df = get_results(r)
+display(df.to_string())
+#df.to_csv(r'C:\Users\44742\Documents\Programs\Git\RedditAPI', header=None, index=None, sep=' ', mode='a')
+df.to_csv('reddit_data.csv')
+#posts = get_post_titles(r)
